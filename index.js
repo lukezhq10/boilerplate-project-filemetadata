e207e86d63984f67771ba97c024ff5a73ccc7aa7
Luke Zhang <you@example.com>
2023-04-07 18:48:10 -0600

    initial commit

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
+const multer = require('multer');
+const upload = multer({ dest: '' }); // default dir for temporary files
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +13,18 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// /api/fineanalyse post req returns json response
+app.post('/api/fileanalyse', upload.single("upfile"), (req, res) => {
+  var fileName = req.file.originalname;
+  var fileType = req.file.mimetype;
+  var fileSize = req.file.size;
 
-
+  res.json({
+    name: fileName,
+    type: fileType,
+    size: fileSize
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
